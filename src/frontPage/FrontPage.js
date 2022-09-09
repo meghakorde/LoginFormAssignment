febaa5b13acd43e6cd503982f071bc1575d7c3fe
meghakorde <megha.perficient@gmail.com>
2022-09-09 23:50:22 +0530

    updated changes

---
+++ src/frontPage/FrontPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const FrontPage = () => {
+  return (
+    <div>
+      <h1>Asension Online Care</h1>
+      <p>
+        Talk to doctor 24/7,anytime anywhere for your urgent needs.Have
+        questions before you get started
+        <a> Learn more</a>
+      </p>
+      <div className="d-flex text-center"></div>
+    </div>
+  );
+};
+
+export default FrontPage;
